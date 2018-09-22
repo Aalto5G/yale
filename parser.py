63a1b990375640dc3cebf96494561e0ccabefc7b
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2018-09-22 09:51:00 +0300

    Opimize parser generator code to not recompile REs

--- parser.py
+++ parser.py
@@ -422,9 +422,10 @@ struct %s_parserstatetblentry {
     #
     for X in sorted(nonterminals):
       sorted_reidx_set = sorted([x for x in terminals if T[X][x]])
-      re_list = list([self.re_by_idx[idx] for idx in sorted_reidx_set])
-      dfa = regex.nfa2dfa(regex.re_compilemulti(*re_list).nfa())
-      regex.set_accepting(dfa, priorities)
+      #re_list = list([self.re_by_idx[idx] for idx in sorted_reidx_set])
+      #dfa = regex.nfa2dfa(regex.re_compilemulti(*re_list).nfa())
+      #regex.set_accepting(dfa, priorities)
+      dfa = self.rec.dfa_by_reidx_set[frozenset(sorted_reidx_set)]
       for x in sorted(terminals):
         if Tt[X][x] == None or Tt[X][x][1] == None:
           continue
@@ -460,9 +461,10 @@ struct %s_parserstatetblentry {
         elif type(rhsitem) == WrapCB:
           x = rhsitem.token
           sortex_reidx_set = sorted([x])
-          re_list = list([self.re_by_idx[idx] for idx in sorted_reidx_set])
-          dfa = regex.nfa2dfa(regex.re_compilemulti(*re_list).nfa())
-          regex.set_accepting(dfa, priorities)
+          #re_list = list([self.re_by_idx[idx] for idx in sorted_reidx_set])
+          #dfa = regex.nfa2dfa(regex.re_compilemulti(*re_list).nfa())
+          #regex.set_accepting(dfa, priorities)
+          dfa = self.rec.dfa_by_reidx_set[frozenset(sorted_reidx_set)]
           regex.check_cb(dfa, x)
           print >>sio, ".rhs =", rhsitem.token, ",", ".cb = ", callbacks_by_name[rhsitem.cbname],
         else:
