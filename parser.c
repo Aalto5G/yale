3ba82544a3ca627c8fd5710b958b209092c34739
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2018-09-24 01:11:26 +0300

    Lots of new parser code

--- parser.c
+++ parser.c
@@ -1,4 +1,5 @@
 #include "yale.h"
+#include <sys/uio.h>
 
 struct bitset {
   uint64_t bitset[4];
@@ -144,4 +145,168 @@ int firstset_issubset(struct dict *da, struct dict *db)
     }
   }
   return 1;
+}
+
+struct REGenEntry {
+  struct bitset key;
+  struct dfa_node *dfas;
+  uint8_t dfacnt;
+};
+
+struct REGen {
+  struct REGenEntry entries[255];
+};
+
+struct LookupTblEntry {
+  uint8_t val;
+  uint8_t cb;
+};
+
+struct ParserGen {
+  struct iovec re_by_idx[255];
+  int priorities[255];
+  uint8_t tokencnt;
+  uint8_t nonterminalcnt;
+  char *parsername;
+  uint8_t start_state;
+  uint8_t epsilon;
+  char *state_include_str;
+  int tokens_finalized;
+  struct rule rules[255];
+  uint8_t rulecnt;
+  struct cb cbs[255];
+  uint8_t cbcnt;
+  uint8_t max_stack_size;
+  struct REGen re_gen;
+  struct LookupTblEntry T[255][255]; // val==255: invalid, cb==255: no callback
+};
+
+void parsergen_init(struct ParserGen *gen, char *parsername)
+{
+  gen->tokencnt = 0;
+  gen->nonterminalcnt = 0;
+  gen->parsername = strdup(parsername);
+  gen->state_include_str = NULL;
+  gen->start_state = 255;
+  gen->epsilon = 255;
+  gen->rulecnt = 0;
+  gen->cbcnt = 0;
+  gen->tokens_finalized = 0;
+  gen->max_stack_size = 0;
+  memset(&gen->re_gen, 0, sizeof(gen->re_gen));
+  memset(gen->T, 0xff, sizeof(gen->T));
+}
+
+void parsergen_state_include(struct ParserGen *gen, char *stateinclude)
+{
+  if (gen->state_include_str != NULL || stateinclude == NULL)
+  {
+    abort();
+  }
+  gen->state_include_str = strdup(stateinclude);
+}
+
+void parsergen_set_start_state(struct ParserGen *gen, uint8_t start_state)
+{
+  if (gen->start_state != 255 || start_state == 255)
+  {
+    abort();
+  }
+  gen->start_state = start_state;
+}
+
+void *memdup(void *base, size_t sz)
+{
+  void *result = malloc(sz);
+  memcpy(result, base, sz);
+  return result;
+}
+
+uint8_t parsergen_add_token(struct ParserGen *gen, char *re, size_t resz, int prio)
+{
+  if (gen->tokens_finalized)
+  {
+    abort();
+  }
+  if (gen->tokencnt >= 255)
+  {
+    abort();
+  }
+  gen->re_by_idx[gen->tokencnt].iov_base = memdup(re, resz);
+  gen->re_by_idx[gen->tokencnt].iov_len = resz;
+  gen->priorities[gen->tokencnt] = prio;
+  return gen->tokencnt++;
+}
+
+void parsergen_finalize_tokens(struct ParserGen *gen)
+{
+  if (gen->tokens_finalized)
+  {
+    abort();
+  }
+  gen->tokens_finalized = 1;
+}
+
+uint8_t parsergen_add_nonterminal(struct ParserGen *gen)
+{
+  if (!gen->tokens_finalized)
+  {
+    abort();
+  }
+  if (gen->tokencnt + gen->nonterminalcnt >= 255)
+  {
+    abort();
+  }
+  return gen->nonterminalcnt++;
+}
+
+int parsergen_is_nonterminal(struct ParserGen *gen, uint8_t x)
+{
+  if (!gen->tokens_finalized)
+  {
+    abort();
+  }
+  return x < gen->tokencnt;
+}
+
+int parsergen_is_rhs_nonterminal(struct ParserGen *gen, const struct ruleitem *rhs)
+{
+  if (!gen->tokens_finalized)
+  {
+    abort();
+  }
+  if (rhs->is_action)
+  {
+    return 0;
+  }
+  return parsergen_is_nonterminal(gen, rhs->value);
+}
+
+void parsergen_set_rules(struct ParserGen *gen, const struct rule *rules, uint8_t rulecnt, const struct namespaceitem *ns)
+{
+  uint8_t i;
+  uint8_t j;
+  gen->rulecnt = rulecnt;
+  for (i = 0; i < rulecnt; i++)
+  {
+    gen->rules[i] = rules[i];
+    for (j = 0; j < gen->rules[i].itemcnt; j++)
+    {
+      gen->rules[i].rhs[j].value = ns[gen->rules[i].rhs[j].value].val;
+    }
+    for (j = 0; j < gen->rules[i].noactcnt; j++)
+    {
+      gen->rules[i].rhsnoact[j].value = ns[gen->rules[i].rhsnoact[j].value].val;
+    }
+  }
+}
+
+void parsergen_set_cb(struct ParserGen *gen, const struct cb *cbs, uint8_t cbcnt)
+{
+  uint8_t i;
+  gen->cbcnt = cbcnt;
+  for (i = 0; i < cbcnt; i++)
+  {
+    gen->cbs[i] = cbs[i];
+  }
 }
