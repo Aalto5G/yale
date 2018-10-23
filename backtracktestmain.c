937ad6f0c0627184baa042ca0b69477146a7554a
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2018-10-23 12:10:50 +0300

    More thorough backtrack test

--- backtracktestmain.c
+++ backtracktestmain.c
@@ -3,15 +3,51 @@
 
 int main(int argc, char **argv)
 {
-  char input[] = "abcdegi";
+  char input1[] = "abcdegi";
+  char input2[] = "abcdefbcdegi";
+  char input3[] = "abcdeghcdegi";
+  char input4[] = "abcdefbcdeghcdegi";
   struct backtracktest_parserctx pctx = {};
   ssize_t consumed;
   size_t i;
 
   backtracktest_parserctx_init(&pctx);
-  for (i = 0; i < sizeof(input)-1; i++)
+  for (i = 0; i < sizeof(input1)-1; i++)
   {
-    consumed = backtracktest_parse_block(&pctx, &input[i], 1);
+    consumed = backtracktest_parse_block(&pctx, &input1[i], 1);
+    if (consumed != 1 && consumed != -EAGAIN)
+    {
+      printf("Fail i = %zu\n", i);
+      abort();
+    }
+    printf("Succeed i = %zu\n", i);
+  }
+  backtracktest_parserctx_init(&pctx);
+  for (i = 0; i < sizeof(input2)-1; i++)
+  {
+    consumed = backtracktest_parse_block(&pctx, &input2[i], 1);
+    if (consumed != 1 && consumed != -EAGAIN)
+    {
+      printf("Fail i = %zu\n", i);
+      abort();
+    }
+    printf("Succeed i = %zu\n", i);
+  }
+  backtracktest_parserctx_init(&pctx);
+  for (i = 0; i < sizeof(input3)-1; i++)
+  {
+    consumed = backtracktest_parse_block(&pctx, &input3[i], 1);
+    if (consumed != 1 && consumed != -EAGAIN)
+    {
+      printf("Fail i = %zu\n", i);
+      abort();
+    }
+    printf("Succeed i = %zu\n", i);
+  }
+  backtracktest_parserctx_init(&pctx);
+  for (i = 0; i < sizeof(input4)-1; i++)
+  {
+    consumed = backtracktest_parse_block(&pctx, &input4[i], 1);
     if (consumed != 1 && consumed != -EAGAIN)
     {
       printf("Fail i = %zu\n", i);
