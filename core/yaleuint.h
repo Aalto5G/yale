087e3939016efa2afaeebaba885743f4033b36dc
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2026-04-14 20:20:29 +0300

    Set YALE_UINT_MAX_LEGAL to 8191, use 16-bit integers

--- core/yaleuint.h
+++ core/yaleuint.h
@@ -3,12 +3,12 @@
 
 #include <stdint.h>
 
-typedef uint8_t yale_uint_t;
+typedef uint16_t yale_uint_t;
 
 // Don't use this, use YALE_UINT_MAX_LEGAL instead
-#define YALE_UINT_MAX UINT8_MAX
+#define YALE_UINT_MAX UINT16_MAX
 
 // This plus 1 must be multiple of 64, this must be at least 255
-#define YALE_UINT_MAX_LEGAL UINT8_MAX
+#define YALE_UINT_MAX_LEGAL 8191
 
 #endif
