c0190c33c73a12f6b309f6869d5460cd0d3d09cb
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2026-04-17 18:41:05 +0300

    Towards selected 8/16-bittiness

--- runtime/yalecommon.h
+++ runtime/yalecommon.h
@@ -21,6 +21,14 @@ enum yale_flags {
   YALE_FLAG_MAJOR_MISTAKE = (1<<3),
 };
 
+typedef uint8_t lexer_uint8_t;
+typedef uint8_t parser_uint8_t;
+#define LEXER_UINT8_MAX UINT8_MAX
+#define PARSER_UINT8_MAX UINT8_MAX
+typedef uint16_t lexer_uint16_t;
+typedef uint16_t parser_uint16_t;
+#define LEXER_UINT16_MAX UINT16_MAX
+#define PARSER_UINT16_MAX UINT16_MAX
 typedef uint8_t lexer_uint_t;
 typedef uint8_t parser_uint_t;
 #define LEXER_UINT_MAX UINT8_MAX
@@ -49,6 +57,58 @@ struct state {
   lexer_uint_t transitions[256];
 #endif
 };
+struct state_p8l8 {
+  uint8_t accepting;
+  uint8_t finalflag;
+  parser_uint8_t acceptid;
+  const parser_uint8_t *taintids;
+  parser_uint8_t taintidsz;
+  uint64_t fastpathbitmask[4];
+#ifdef SMALL_CODE
+  const lexer_uint8_t *transitions;
+#else
+  lexer_uint8_t transitions[256];
+#endif
+};
+struct state_p8l16 {
+  uint8_t accepting;
+  uint8_t finalflag;
+  parser_uint8_t acceptid;
+  const parser_uint8_t *taintids;
+  parser_uint8_t taintidsz;
+  uint64_t fastpathbitmask[4];
+#ifdef SMALL_CODE
+  const lexer_uint16_t *transitions;
+#else
+  lexer_uint16_t transitions[256];
+#endif
+};
+struct state_p16l8 {
+  uint8_t accepting;
+  uint8_t finalflag;
+  parser_uint16_t acceptid;
+  const parser_uint16_t *taintids;
+  parser_uint16_t taintidsz;
+  uint64_t fastpathbitmask[4];
+#ifdef SMALL_CODE
+  const lexer_uint8_t *transitions;
+#else
+  lexer_uint8_t transitions[256];
+#endif
+};
+struct state_p16l16 {
+  uint8_t accepting;
+  uint8_t finalflag;
+  parser_uint16_t acceptid;
+  const parser_uint16_t *taintids;
+  parser_uint16_t taintidsz;
+  uint64_t fastpathbitmask[4];
+#ifdef SMALL_CODE
+  const lexer_uint16_t *transitions;
+#else
+  lexer_uint16_t transitions[256];
+#endif
+};
 
 struct callbacks {
   uint64_t cbsmask;
@@ -62,15 +122,45 @@ struct ruleentry {
   parser_uint_t rhs;
   parser_uint_t cb;
 };
+struct ruleentry8 {
+  parser_uint8_t rhs;
+  parser_uint8_t cb;
+};
+struct ruleentry16 {
+  parser_uint16_t rhs;
+  parser_uint16_t cb;
+};
 
 struct rule {
   parser_uint_t lhs;
   parser_uint_t rhssz;
   const struct ruleentry *rhs;
 };
+struct rule8 {
+  parser_uint8_t lhs;
+  parser_uint8_t rhssz;
+  const struct ruleentry8 *rhs;
+};
+struct rule16 {
+  parser_uint16_t lhs;
+  parser_uint16_t rhssz;
+  const struct ruleentry16 *rhs;
+};
 
 struct reentry {
   const struct state *re;
+};
+struct reentry_p8l8 {
+  const struct state_p8l8 *re;
+};
+struct reentry_p8l16 {
+  const struct state_p8l16 *re;
+};
+struct reentry_p16l8 {
+  const struct state_p16l8 *re;
+};
+struct reentry_p16l16 {
+  const struct state_p16l16 *re;
 };
 
 #endif
